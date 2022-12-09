f1f3b7fd6e07e41a3f6280ae4f849bef94a79db0
Lungten Wangchuk <thelungten@Lungtens-MacBook-Pro.local>
2022-12-09 02:02:04 +0000

    Initial commit

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,6 +1,7 @@
 function Icons() {
   return (
     <div className="pt-2 flex justify-center space-x-1">
+      {/* logos */}
       <img
         className="w-10"
         src="src/assets/icons8-react.svg"
